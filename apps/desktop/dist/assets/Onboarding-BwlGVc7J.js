05802b3946e30de9ff4a0a7be3dccc972d705917
Ezzy Rappeport <admin@flowe.cc>
2026-07-12 21:14:57 -0700

    chore(desktop): refresh renderer bundle

--- apps/desktop/dist/assets/Onboarding-BwlGVc7J.js
+++ apps/desktop/dist/assets/Onboarding-BwlGVc7J.js
@@ -1,4 +1,4 @@
-import{c as g,r as c,b as h,u as L,f as m,g as H,j as e,X as $,e as j,C as _,T as b,F as I,h as V}from"./index-DStSKuke.js";import{W as C}from"./wand-sparkles-Wuanmfk1.js";/**
+import{c as g,r as c,b as h,u as L,f as m,g as H,j as e,X as $,e as j,C as _,T as b,F as I,h as V}from"./index-qNdQtv_J.js";import{W as C}from"./wand-sparkles-Cz-nGs-M.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- apps/desktop/dist/assets/rotate-ccw-CC5Bsjbx.js
+++ apps/desktop/dist/assets/rotate-ccw-CC5Bsjbx.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-DStSKuke.js";/**
+import{c as t}from"./index-qNdQtv_J.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
